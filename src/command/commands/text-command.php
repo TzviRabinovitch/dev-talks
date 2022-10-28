04d6c10a5795bead288a812321c9cf39a0fe5a12
StyleShit <evyatar.daud@gmail.com>
2022-10-28 14:01:16 +0300

    WIP

--- src/command/commands/text-command.php
+++ src/command/commands/text-command.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . "/../interfaces/command-Interface.php";
+require_once __DIR__ . "/../interfaces/command-interface.php";
 
 class TextCommand implements CommandInterface
 {
