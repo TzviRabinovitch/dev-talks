74983b7ee0adffd0a32455650108ce29c1da785c
StyleShit <evyatar.daud@gmail.com>
2022-10-28 13:46:44 +0300

    Dependency Injection Container

---
+++ src/dependency-injection/di-container.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Src\DependencyInjectio;
+
+class DiContainer {
+    protected $bindings = [];
+
+    protected $instances = [];
+
+    public function bind( $abstract, $resolver = null, $shared = false ) {
+        // By default, try auto resolving a concrete from the given abstract.
+        if ( ! $resolver ) {
+            $resolver = function ( DiContainer $container, $args ) use ( $abstract ) {
+                return $container->makeWithDependencies( $abstract, $args );
+            };
+        }
+
+        $this->bindings[ $abstract ] = [
+            'resolver' => $resolver,
+            'shared' => $shared,
+        ];
+
+        return $this;
+    }
+
+    public function singleton( $abstract, $resolver = null ) {
+        return $this->bind( $abstract, $resolver, true );
+    }
+
+    public function has( $abstract ) {
+        return isset( $this->bindings[ $abstract ] );
+    }
+
+    public function make( $abstract, $args = [] ) {
+        // Try to automatically make an abstract even if it's not bound.
+        if ( ! $this->has( $abstract ) ) {
+            if ( ! class_exists( $abstract ) ) {
+                throw new \Exception( "Abstract `$abstract::class` not found" );
+            }
+            
+            return $this->makeWithDependencies( $abstract, $args );
+        }
+
+        $binding = $this->bindings[ $abstract ];
+        $resolve = $binding['resolver'];
+
+        // Singletons.
+        if ( $binding['shared'] ) {
+            if ( ! isset( $this->instances[ $abstract ] ) ) {
+                $this->instances[ $abstract ] = $resolve( $this, $args );
+            }
+            
+            return $this->instances[ $abstract ];
+        }
+        
+        // Non-Singletons.
+        return $resolve( $this, $args );
+    }
+
+    protected function makeWithDependencies( $abstract, $args ) {
+        $dependencies = $this->resolveDependencies( $abstract );
+
+        $dependencies = array_map( function ( \ReflectionParameter $dep ) use ( $args ) {
+            if ( isset( $args[ $dep->getName() ] ) ) {
+                return $args[ $dep->getName() ];
+            }
+
+            if ( $dep->isDefaultValueAvailable() ) {
+                return $dep->getDefaultValue();
+            }
+
+            return $this->make( $dep->getType()->getName() );
+        }, $dependencies );
+
+        return new $abstract( ...$dependencies );
+    }
+
+    protected function resolveDependencies( $abstract ) {
+        if ( ! interface_exists( $abstract ) && ! class_exists( $abstract ) ) {
+            throw new \Exception( "Abstract `$abstract::class` not found" );
+        }
+
+        $reflection = new \ReflectionClass( $abstract );
+        $constructor = $reflection->getConstructor();
+
+        if ( ! $constructor ) {
+            return [];
+        }
+
+        return $constructor->getParameters();
+    }
+}
